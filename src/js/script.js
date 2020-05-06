7101c401591325b6d7bc94bbf6ab0444d74c1f29
Viktor Chernenko <chernenkoviktor1991@gmail.com>
2020-05-06 18:23:19 +0300

    practice

---
+++ src/js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // === nav-mobile ===
+
+    if ($(".nav-mobile").length > 0) {
+      var btn_nav_mobile = $('.nav-mobile').find('.nav-btn'),
+          nav_mobile = $('.nav-mobile').find('.nav-mobile-container'),
+          nav_link = $('.nav-mobile').find('.nav__link');
+
+      btn_nav_mobile.click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active');
+        nav_mobile.toggleClass('nav-mobile-container_active');
+      });
+
+      nav_link.click(function (i) {
+        i.preventDefault();
+        btn_nav_mobile.removeClass('active');
+        nav_mobile.removeClass('nav-mobile-container_active');
+      });
+    } 
+    // === / nav-mobile ===
+
+    // === Scroll top ===
+    
+    $(function(){
+        $.fn.scrollToTop=function(){
+          $(this).hide().removeAttr("href");
+          if($(window).scrollTop()!="0"){
+              $(this).fadeIn("slow")
+        }
+        var scrollDiv=$(this);
+        $(window).scroll(function(){
+          if($(window).scrollTop()=="0"){
+          $(scrollDiv).fadeOut("slow")
+          }else{
+          $(scrollDiv).fadeIn("slow")
+        }
+        });
+          $(this).click(function(){
+            $("html, body").animate({scrollTop:0},"slow")
+          })
+        }
+      });
+      $(function() {$("#scroll-top").scrollToTop();});
+
+    // === / Scroll top ===
+    
+});
